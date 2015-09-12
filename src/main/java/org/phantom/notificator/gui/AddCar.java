1226475174f32a5a1b75682109ba6d08228f6000
mihnea17 <mihnea_pat@yahoo.com>
2015-09-12 10:05:58 +0300

    Added validation checking & messaging & cleaned code for AddCar

--- src/main/java/org/phantom/notificator/gui/AddCar.java
+++ src/main/java/org/phantom/notificator/gui/AddCar.java
@@ -7,98 +7,128 @@ import org.phantom.notificator.mappers.CarMapper;
 import org.phantom.notificator.mappers.CarOwnerMapper;
 
 import javax.swing.*;
+import javax.validation.ConstraintViolation;
 import java.awt.*;
-import java.awt.event.*;
-import java.awt.font.TextAttribute;
-import java.util.Map;
+import java.util.Set;
 
 /**
  * Created by mihne_000 on 6/30/2015.
  */
-public class AddCar extends JFrame{
-    private JPanel panel1;
-    private JTextField textField1;
-    private JTextField textField2;
-    private JTextField textField3;
-    private JTextField textField4;
-    private JTextField textField5;
-    private JTextField textField6;
-    private JTextField textField7;
+public class AddCar extends JFrame {
+    private static final String NEW_LINE = "\n";
+    private final CarOwnerMapper carOwnerMapper;
+    private final CarMapper carMapper;
+    private JPanel panel;
+    private JTextField carRegistrationNumberTextField;
+    private JTextField itpExpiryDateTextField;
+    private JTextField firstNameTextField;
+    private JTextField lastNameTextField;
+    private JTextField companyNameTextField;
+    private JTextField emailTextField;
+    private JTextField telephoneNoTextField;
     private JButton addCarButton;
     private JButton backButton;
-    private JButton nextButton;
+    private JButton adaugaMasinaButton;
     private JLabel detaliiClientLabel;
+    private static final Dimension PREFERRED_SIZE = new Dimension(500, 500);
 
-    public AddCar(CarMapper carMapper,CarOwnerMapper carOwnerMapper){
+    public AddCar(CarMapper carMapper, CarOwnerMapper carOwnerMapper) {
         super("Add Cars");
-        add(panel1);
+        this.carMapper = carMapper;
+        this.carOwnerMapper = carOwnerMapper;
+        add(panel);
+        setUpButtonListeners();
+        setPreferredSize(PREFERRED_SIZE);
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        backButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new MainMenu2(carMapper, carOwnerMapper);
-                setVisible(false);
-            }
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    private void setUpButtonListeners() {
+        backButton.addActionListener(e -> {
+            new MainMenu(carMapper, carOwnerMapper);
+            setVisible(false);
         });
-        /*nextButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                new ConfirmDetails(carMapper,carOwnerMapper);
-                setVisible(false);
-            }
-        });*/
-        addCarButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                addCar(carOwnerMapper,carMapper);
-            }
-        });
-        KeyboardFocusManager.getCurrentKeyboardFocusManager()
-                .addKeyEventDispatcher(new KeyEventDispatcher() {
-                    @Override
-                    public boolean dispatchKeyEvent(KeyEvent e) {
-                        if(e.getKeyCode()==KeyEvent.VK_ENTER) {
-                            addCar(carOwnerMapper,carMapper);
-                            return false;
-                        }
-                        return false;
-                    }
-                });
+        adaugaMasinaButton.addActionListener(e -> addCarFromUserInput());
     }
 
     private void createUIComponents() {
-        // TODO: place custom component creation code here
-        panel1=new JPanel(new BorderLayout());
-        panel1.setPreferredSize(new Dimension(500, 500));
-        panel1.setFocusable(true);
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int height = screenSize.height;
-        int width = screenSize.width;
-        setSize(width / 2, height / 2);
+        panel = new JPanel(new BorderLayout());
+        panel.setFocusable(true);
     }
-    private void addCar(CarOwnerMapper carOwnerMapper, CarMapper carMapper)
-    {
-        String regNo = textField1.getText();
-        String calendar = textField2.getText();
-        String fname = textField3.getText();
-        String lname = textField4.getText();
-        String company = textField5.getText();
-        String email = textField6.getText();
-        String tel = textField7.getText();
-        CarOwner prop = new CarOwner(fname, lname, tel);
-        carOwnerMapper.addCarOwner(prop);
-        carMapper.addCarToOwner(prop, new Car(regNo, new LocalDate(calendar), prop));
-        if(carMapper.isRegistrationNumberInDb(regNo))
-        {
-            new Popup(carMapper,carOwnerMapper);
+
+    private void addCarFromUserInput() {
+        LocalDate itpExpiryDate;
+        try {
+            itpExpiryDate = LocalDate.parse(itpExpiryDateTextField.getText());
+        } catch (IllegalArgumentException exception) {
+            JOptionPane.showMessageDialog(panel, "Data Expirare ITP este incorecta!\n" +
+                            "Data trebuie sa fie de forma yyyy-MM-dd (Exemplu: 2015-01-24)",
+                    "Data Expirare ITP incorecta",
+                    JOptionPane.ERROR_MESSAGE);
+            return;
         }
-        else
-        {
-            new MainMenu2(carMapper,carOwnerMapper);
-            setVisible(false);
+
+        // Get the other text field values
+        String carRegistrationNumber = carRegistrationNumberTextField.getText().toUpperCase();
+        String firstName = firstNameTextField.getText();
+        String lastName = lastNameTextField.getText();
+        String companyName = companyNameTextField.getText();
+        String email = emailTextField.getText();
+        String telephoneNo = telephoneNoTextField.getText();
+
+        // Create owner and car
+        CarOwner owner = new CarOwner(firstName, lastName, telephoneNo);
+        if (!email.isEmpty()) {
+            owner.setEmail(email);
+        }
+        if (!companyName.isEmpty()) {
+            owner.setCompanyName(companyName);
+        }
+        Car carToAdd = new Car(carRegistrationNumber, itpExpiryDate, owner);
+
+        validateAndInsert(owner, carToAdd);
+    }
+
+    private void validateAndInsert(CarOwner owner, Car carToAdd) {
+        Set<ConstraintViolation<CarOwner>> carOwnerValidationErrors = carOwnerMapper.getValidationErrorSet(owner);
+        Set<ConstraintViolation<Car>> carValidationErrors = carMapper.isValidCar(carToAdd);
+
+        if (carOwnerValidationErrors.isEmpty() && carValidationErrors.isEmpty()) {
+            boolean isCarAdded = carOwnerMapper.addCarToOwner(owner, carToAdd);
+            displayInsertionStatus(isCarAdded, carToAdd.getCarRegistrationNumber());
+        } else {
+            StringBuilder errorMessageForDisplay = getValidationErrorMessageForDisplay(carOwnerValidationErrors, carValidationErrors);
+            JOptionPane.showMessageDialog(panel, errorMessageForDisplay,
+                    "Detalii Client/Masina incorecte!",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private StringBuilder getValidationErrorMessageForDisplay(Set<ConstraintViolation<CarOwner>> carOwnerValidationErrors, Set<ConstraintViolation<Car>> carValidationErrors) {
+        StringBuilder errorMessageForDisplay = new StringBuilder();
+        for (ConstraintViolation<Car> a : carValidationErrors) {
+            errorMessageForDisplay.append(a.getMessage()).append(NEW_LINE);
+        }
+        for (ConstraintViolation<CarOwner> a : carOwnerValidationErrors) {
+            errorMessageForDisplay.append(a.getMessage()).append(NEW_LINE);
+        }
+        return errorMessageForDisplay;
+    }
+
+    private void displayInsertionStatus(boolean isCarAdded, String carRegistrationNumber) {
+        if (isCarAdded) {
+            JOptionPane.showMessageDialog(panel,
+                    "Masina cu numarul " + carRegistrationNumber + " a fost adaugata in sistem",
+                    "Adaugare reusita!!",
+                    JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(panel,
+                    "Masina cu numarul " + carRegistrationNumber + " NU a fost adaugata in sistem.\n" +
+                            "Verifica log-urile pentru mai multe detalii.",
+                    "Adaugare nereusita!!",
+                    JOptionPane.ERROR_MESSAGE);
         }
     }
 }
