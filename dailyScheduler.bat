08b02a05d163a7085528f5f6e677fbd11f1e9727
Mistuus <victormihai.patentasu10@gmail.com>
2015-09-17 21:33:19 +0300

    Modified .bat file to cd to the right directory; Added Windows Task xml file for importing

--- dailyScheduler.bat
+++ dailyScheduler.bat
@@ -1,3 +1,7 @@
 ECHO Incepem sa trimitem Email/SMS clientilor Vector Truck Management...
+:: Put below the path to the DIRECTORY of the dailyScheduler.bat file
+F:
+cd \Projects\itp-notificator
+:: ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 cmd /k mvn -e compile exec:java -Dexec.mainClass="org.phantom.notificator.schedulers.DailyScheduler"
 ECHO Programul a terminat de trimis mesaje clientilor!
