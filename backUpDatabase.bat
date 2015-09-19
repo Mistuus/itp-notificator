90716598e6e47de4c5f5513363be720aa0a19299
mihnea17 <mihnea_pat@yahoo.com>
2015-09-19 10:11:22 +0300

    Made backUpDatabase.bat

---
+++ backUpDatabase.bat
@@ -0,0 +1,7 @@
+ECHO Efectuam copia de rezerva a bazei de date Vector Truck Management...
+:: Put below the path to the DIRECTORY of the dailyScheduler.bat file
+F:
+cd \Projects\itp-notificator
+:: ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+cmd /k mvn -e compile exec:java -Dexec.mainClass="org\phantom\notificator\util\BackUpDatabaseUtil.java"
+ECHO Programul a terminat de copiat baza de date!
